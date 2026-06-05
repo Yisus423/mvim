c3fa528537bc039ba6e050c2dd5cf5fa561bbdd5
Jesus carrion <jesusalfonsocarrionmendoza23@gmail.com>
2026-06-05 16:52:25 -0400

    feat(config): Create options.lua
    This file contain the next options:
    - Lines and columns:
      * Show line numbers
      * Relative numbers for quick jumps
      * Always show the column simbols (avoid screen jumps)
    - Tabs and indentations:
      * 2 spaces per tab
      * 2 spaces for automatic indentation
      * Convert tabs in spaces
      * Mantain the indentation of prev line
    - Search:
      * Ignore uppercase/lowercase in searchs
      * Case-sensitive if a uppercase is entered
    - Interface:
      * TrueColors of 24 bits
      * Active the mouse support
      * Sync the system clipboard
      * Highlight the current line
      * Mantain the 8 lines margin to scroll

---
+++ lua/config/options.lua
@@ -0,0 +1,23 @@
+local opt = vim.opt
+
+-- Lines and columns
+opt.number = true         -- Show line numbers
+opt.relativenumber = true -- Relative numbers for quick jumps
+opt.signcolumn = "yes"    -- Always show the column simbols (avoid screen jumps)
+
+-- Tabs and indentations
+opt.tabstop = 2           -- 2 spaces per tab
+opt.shiftwidth = 2        -- 2 spaces for automatic indentation
+opt.expandtab = true      -- Convert tabs in spaces
+opt.autoindent = true     -- Mantain the indentation of prev line
+
+-- Search
+opt.ignorecase = true     -- Ignore uppercase/lowercase in searchs
+opt.smartcase = true      -- Case-sensitive if a uppercase is entered
+
+-- Interface
+opt.termguicolors = true  -- TrueColors of 24 bits
+opt.mouse = "a"           -- Active the mouse support
+opt.clipboard = "unnamedplus" -- Sync the system clipboard
+opt.cursorline = true     -- Highlight the current line
+opt.scrolloff = 8         -- Mantain the 8 lines margin to scroll
